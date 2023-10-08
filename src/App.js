67f440bebc5265675a8c0d990bc37dd19595d411
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-08 19:48:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
+import { Button, TextField } from '@mui/material';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className='container'>
+          {/* text*/}
+          <div className='heading'>
+            <h1 className='main-heading'>facebook</h1>
+            <p className='about-fb'>Facebook help you connect and share with the people in your life</p>
+          </div>
+          {/* form */}
+
+          <form>
+
+            <div class="input-form">
+
+            <div class="text-field">
+            <TextField className='outlined-basic' id="outlined-basic" label="Email" variant="outlined" />
+            </div>
+
+            <div class="text-field">
+            <TextField className='outlined-basic' id="outlined-basic" label="Password" variant="outlined" />
+            </div>
+
+           {/* button */}
+           <div className='button-btn'>
+           <button className='btn'>Login</button>
+           </div>
+
+            </div>
+
+            
+            
+
+          </form>
+          
+          
+        </div>
+      </div>
+    </>
   );
 }
 
