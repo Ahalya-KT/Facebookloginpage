cf7cc1aa35baad57e1bc3798a52eae4f683cc772
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-08 21:23:08 +0530

    facebook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Button, TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import './App.css';
 
 
@@ -10,8 +10,9 @@ function App() {
       <div className="app">
         <div className='container'>
           {/* text*/}
-          <div className='heading'>
-            <h1 className='main-heading'>facebook</h1>
+          <div className='headings'>
+            <img  className='mainheading 'src="https://static.xx.fbcdn.net/rsrc.php/yI/r/4aAhOWlwaXf.svg"></img>
+            {/* <h1 className='main-heading'>facebook</h1> */}
             <p className='about-fb'>Facebook help you connect and share with the people in your life</p>
           </div>
           {/* form */}
@@ -21,7 +22,8 @@ function App() {
             <div class="input-form">
 
             <div class="text-field">
-            <TextField className='outlined-basic' id="outlined-basic" label="Email" variant="outlined" />
+            <TextField className='outlined-basic' id="outlined-basic" label="Email address or phone number" variant="outlined" />
+           
             </div>
 
             <div class="text-field">
@@ -29,16 +31,29 @@ function App() {
             </div>
 
            {/* button */}
+
            <div className='button-btn'>
-           <button className='btn'>Login</button>
+           <button className='login-button'>Login</button>
+           </div>
+
+           <div className='forgot-msg'>
+            <h6>Forgotten Password?</h6>
+           </div>
+
+           <div className='new-account'>
+            <button className='create-acctn'> Create new account</button>
+
            </div>
 
             </div>
+ 
 
-            
-            
+            <div className='sectionlast'>
+            <h6>create a page for a celebrity,brand or business</h6>
 
+          </div>
           </form>
+
           
           
         </div>
